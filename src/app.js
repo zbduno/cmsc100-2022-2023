2b3aaf3eee5497e26f92a08be80472552cabdbd2
zbduno <zbduno@up.edu.ph>
2022-11-17 20:19:51 +0000

    feat: add openapi glue and swagger

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 import Fastify from 'fastify';
 import sensible from '@fastify/sensible';
-import { general } from './services/general/index.js';
-import { createTodo } from './services/todos/create-todo.js';
-import { getManyTodo } from './services/todos/get-many-todo.js';
-import { getTodo } from './services/todos/get-todo.js';
-import { updateTodo } from './services/todos/update-todo.js';
-import { deleteTodo } from './services/todos/delete-todo.js';
+import openAPIGlue from 'fastify-openapi-glue';
+import swagger from '@fastify/swagger';
 
 const prefix = '/api';
 
@@ -14,22 +10,31 @@ export async function build () {
   const fastify = Fastify({ logger: true });
   fastify.register(sensible);
 
-  fastify.get(prefix, general);
+  const openAPIGlueOptions = {
+    prefix
+  };
 
-  // create todo
-  fastify.post(`${prefix}/todo`, createTodo);
+  const swaggerOptions = {
+    exposeRoute: true
+  };
 
-  // get many todo
-  fastify.get(`${prefix}/todo`, getManyTodo);
+  fastify.register(swagger, swaggerOptions);
+  fastify.register(openAPIGlue, openAPIGlueOptions);
 
-  // get one todo
-  fastify.get(`${prefix}/todo/:todoId`, getTodo);
+  // // create todo
+  // fastify.post(`${prefix}/todo`, createTodo);
 
-  // update one todo
-  fastify.put(`${prefix}/todo/:todoId`, updateTodo);
+  // // get many todo
+  // fastify.get(`${prefix}/todo`, getManyTodo);
 
-  // delete one todo
-  fastify.delete(`${prefix}/todo/:todoId`, deleteTodo);
+  // // get one todo
+  // fastify.get(`${prefix}/todo/:todoId`, getTodo);
+
+  // // update one todo
+  // fastify.put(`${prefix}/todo/:todoId`, updateTodo);
+
+  // // delete one todo
+  // fastify.delete(`${prefix}/todo/:todoId`, deleteTodo);
 
   return fastify;
-}
+} 
